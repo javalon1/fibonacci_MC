9898c0d5985b4e866ef7b01c4019ffab83a57c90
ciechann <emgasuk@gmail.com>
2019-05-12 00:59:50 +0100

    ciag

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,10 +1,22 @@
 package com.sda.algorytmy;
 
-/**
- * Hello world!
- */
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println(ciagF(11));
+
+    }
+
+    public static int ciagF(int a) {
+
+        if (a == 0) {
+            return 0;
+        }
+        if (a == 1) {
+            return 1;
+        }
+        return ciagF(a - 1) + ciagF(a - 2);
     }
 }
+
