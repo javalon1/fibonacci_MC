5d440b7a031df1bd5373c21f8de1a41b4bee0bfe
ciechann <emgasuk@gmail.com>
2019-05-12 11:39:44 +0100

    ostatecznie

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,15 +1,22 @@
 package com.sda.algorytmy;
 
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
 
-      System.out.println(ciagF(11));
-      ciagFr(11);
-        ciagFr2(2);
+        System.out.println("Ciaga F rekurencyjnie: " + ciagFr(11));
+        System.out.println("Ciag F iteracyjnie z tablic: ");
+        ciagFt(11);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj liczbe ciagu F: ");
+        int x = scan.nextInt();
+        System.out.println("Ciag iteracyjnie z tej liczby: ");
+        ciagFnT(x);
     }
 
-    public static int ciagF(int a) {
+    public static int ciagFr(int a) {
 
         if (a == 0) {
             return 0;
@@ -17,37 +24,31 @@ public class App {
         if (a == 1) {
             return 1;
         }
-        return ciagF(a - 1) + ciagF(a - 2);
+        return ciagFr(a - 1) + ciagFr(a - 2);
     }
 
-    public static void ciagFr(int a) {
+    public static void ciagFt(int a) {
 
-        int[] tab = new int[20];
+        int[] tab = new int[a+1];
         tab[0] = 0;
         tab[1] = 1;
         for (int i = 2; i < tab.length; i++) {
             tab[i] = tab[i - 1] + tab[i - 2];
             System.out.println(tab[i]);
         }
-
     }
 
-    public static void ciagFr2(int x) {
-
+    public static void ciagFnT(int x) {
         if (x < 2) {
             System.out.println(x);
         }
-
         int a = 0;
         int b = 1;
         int wynik = 1;
-
         for (int i = 2; i < x; i++) {
-
             a = b;
             b = wynik;
             wynik = a + b;
-
         }
         System.out.println(wynik);
     }
