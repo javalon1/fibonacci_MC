8c5b82ae65e5c271be9bf716cfcb633b4a21043a
ciechann <emgasuk@gmail.com>
2019-05-12 09:57:14 +0100

    iteracja

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args) {
 
         System.out.println(ciagF(11));
-
+        ciagFr(11);
     }
 
     public static int ciagF(int a) {
@@ -17,6 +17,18 @@ public class App {
             return 1;
         }
         return ciagF(a - 1) + ciagF(a - 2);
+    }
+
+    public static void ciagFr(int a) {
+
+        int[] tab = new int[20];
+        tab[0] = 0;
+        tab[1] = 1;
+        for (int i = 2; i < tab.length; i++) {
+            tab[i] = tab[i - 1] + tab[i - 2];
+            System.out.println(tab[i]);
+        }
+
     }
 }
 
